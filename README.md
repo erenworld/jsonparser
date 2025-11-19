69d77ce61418ac5b830f93bdc26e8927732344ea
erenworld <erenxturkoglu@gmail.com>
2025-11-19 21:18:50 +0100

    Add str_next_occurence

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # jsonparser
+
 Json parser in C
+
+## Features
+
+- Structured Data (JSONObject, JSONPair, JSONValue)
+- Recursive JSON parsing
+- Count of key-value pairs of current JSON
+
+## Usage
+
+```c
+
+#include "json.h"
+
+int main()
+{
+    const char *json_string = "{\"hello\":\"world\",\"key\":\"value\"}";
+
+    json_object *json = parse_json(json_string);
+    if (json == NULL) {
+        return -1;
+    }
+
+    printf("Count: %i\n", json->count);
+    
+}
+
+```
--- json.c
+++ json.c
@@ -1,13 +1,31 @@
 #include "json.h"
 
-JSON_OBJECT *parseJSON(string json_string) {
-    int32_t offset = 0;
+JSON_OBJECT *parseJSON(string json_string) 
+{
+    int offset = 0;
     JSON_OBJECT *temp_obj = _parseJSON(json_string, &offset);
 
     return temp_obj;
 }
 
-int main() {
+static int str_next_occurence(string str, char ch)
+{
+    int pos = 0;
+
+    if (str == NULL) {
+        return -1;
+    }
+
+    while (*str != ch && *str != '\0') {
+        str++;
+        pos++;
+    }
+
+    return (*str == '\0') ? -1 : pos;
+}
+
+int main() 
+{
     printf("Welcome to Jason!\n");
 
     return 0;
