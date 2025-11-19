3347481e9052ee384b32b652312cb13d57f31046
erenworld <erenxturkoglu@gmail.com>
2025-11-19 20:56:33 +0100

    Add parse json wrapper

--- json.c
+++ json.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
+#include "json.h"
 
+JSON_OBJECT *parseJSON(string json_string) {
+    int32_t offset = 0;
+    JSON_OBJECT *temp_obj = _parseJSON(json_string, &offset);
+
+    return temp_obj;
+}
 
 int main() {
     printf("Welcome to Jason!\n");
